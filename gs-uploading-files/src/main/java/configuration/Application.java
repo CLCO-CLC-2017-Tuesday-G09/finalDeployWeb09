47df89ab11dc267cfc1a1fb741e453a8e5e4ad2b
WELCOME <WELCOME@DESKTOP-B9E2G8P>
2017-05-26 10:30:01 +0700

    cmoit

--- gs-uploading-files/src/main/java/configuration/Application.java
+++ gs-uploading-files/src/main/java/configuration/Application.java
@@ -14,12 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-
-import hello.StorageProperties;
-import hello.StorageService;
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class Application extends SpringBootServletInitializer {
 	static String RESOURCES_DIR;
 	public static void main(String[] args) {
@@ -30,21 +25,4 @@ public class Application extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-		storageService.deleteAll();
-		storageService.init(); 
-		};
-}
-	@Override
-    public void onStartup(ServletContext container) {
-      XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-      appContext.setConfigLocation("/WEB-INF/spring/springmvc-config.xml");
-
-      ServletRegistration.Dynamic dispatcher =
-        container.addServlet("dispatcher", new DispatcherServlet(appContext));
-      dispatcher.setLoadOnStartup(1);
-      dispatcher.addMapping("/");
-    }
 }
